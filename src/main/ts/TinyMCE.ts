29d4e3d6e28a016e47118f2db8b5e3cc3ca00f56
王凡 <18516043477@xtransfer.cn>
2023-06-24 22:59:01 +0800

    fix: patch for qiankun

--- src/main/ts/TinyMCE.ts
+++ src/main/ts/TinyMCE.ts
@@ -1,9 +1,13 @@
-import { TinyMCE as TinyMCEGlobal } from 'tinymce';
+import { TinyMCE as TinyMCEGlobal } from 'tinymce'
 
 const getTinymce = (view: Window): TinyMCEGlobal | null => {
-  const global = view as any;
+	const myWindow = window as any
+	if (myWindow?.__POWERED_BY_QIANKUN__ && myWindow?.tinymce && !(view as any)?.tinymce) {
+		// for qiankun
+		return myWindow.tinymce
+	}
+	const global = view as any
+	return global && global.tinymce ? global.tinymce : null
+}
 
-  return global && global.tinymce ? global.tinymce : null;
-};
-
-export { getTinymce };
+export { getTinymce }
