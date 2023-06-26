457833b0247effa7b646a017d164dfc1b591bf83
王凡 <18516043477@xtransfer.cn>
2023-06-26 17:28:04 +0800

    fix: ignore something

--- src/main/ts/TinyMCE.ts
+++ src/main/ts/TinyMCE.ts
@@ -1,13 +1,16 @@
+/* eslint-disable no-underscore-dangle */
 import { TinyMCE as TinyMCEGlobal } from 'tinymce'
 
+type MyWindow = Window & { __POWERED_BY_QIANKUN__?: boolean; tinymce?: TinyMCEGlobal }
+
 const getTinymce = (view: Window): TinyMCEGlobal | null => {
-	const myWindow = window as any
-	if (myWindow?.__POWERED_BY_QIANKUN__ && myWindow?.tinymce && !(view as any)?.tinymce) {
-		// for qiankun
-		return myWindow.tinymce
-	}
-	const global = view as any
-	return global && global.tinymce ? global.tinymce : null
+  const myWindow = window as unknown as MyWindow
+  if (myWindow?.__POWERED_BY_QIANKUN__ && myWindow?.tinymce && !(view as MyWindow)?.tinymce) {
+    // for qiankun
+    return myWindow.tinymce
+  }
+  const global = view as any
+  return global && global.tinymce ? global.tinymce : null
 }
 
 export { getTinymce }
